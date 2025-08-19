faa4ac6cf0df663ca66d9f804c8a180d873ffb28
Mastewal <mastewalgshiferaw@gmail.com>
2025-08-19 04:07:50 +0300

    Implemented a user registration and JWT authentication endpoints

--- habits/views.py
+++ habits/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics
+from .serializers import UserSerializer
 
-# Create your views here.
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [] # No permission needed to register
